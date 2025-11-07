46c7a2c3eea889b6ecf7bbcf83bf86269d1f0f11
YannickNino <yannickaristide.jlg@gmail.com>
2025-11-07 10:58:21 +0100

    Ajout du fichier test de l'Api

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,20 +1,20 @@
 from fastapi.testclient import TestClient
-from app import app
-
-client = TestClient(app)
+from service.app import app
 
 def test_health_endpoint():
-    response = client.get("/health")
-    assert response.status_code == 200
-    data = response.json()
-    assert "status" in data
-    assert data["status"] == "ok"
+    with TestClient(app) as client:
+        response = client.get("/health")
+        assert response.status_code == 200
+        data = response.json()
+        assert "status" in data
+        assert data["status"] == "ready"
 
 def test_predict_endpoint():
-    payload = {"texts": ["I love this product!", "This is awful."]}
-    response = client.post("/predict", json=payload)
-    assert response.status_code == 200
-    data = response.json()
-    assert "scores" in data
-    assert isinstance(data["scores"], list)
-    assert isinstance(data["scores"][0], dict)
+    with TestClient(app) as client:
+        payload = {"texts": ["I love this product!", "This is awful."]}
+        response = client.post("/predict", json=payload)
+        assert response.status_code == 200
+        data = response.json()
+        assert "scores" in data
+        assert isinstance(data["scores"], list)
+        assert isinstance(data["scores"][0], dict)
